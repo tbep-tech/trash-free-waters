c7683df7d89a28528f10e89500a4c4d8cbd0538d
fawda123 <mbafs2012@gmail.com>
2022-06-27 10:30:21 -0400

    rmd, figs, funcs templates

---
+++ R/figs.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(lubridate)
+library(plotly)
+library(colorspace)
+library(reactable)
+library(downloadthis)
+library(tools)
+library(shiny)
+library(crosstalk)
+library(DT)
+library(scales)
+library(here)
+# library(showtext)
+
+data(tfwdat)
+
+# # get font
+# font_add_google("Roboto", "roboto")#, regular = 'C:/Windows/Fonts/Roboto.ttf')
+# fml <- "roboto"
+# 
+# showtext_auto()
+# showtext_opts(dpi = 500)
+
+# first plot --------------------------------------------------------------
+
+
